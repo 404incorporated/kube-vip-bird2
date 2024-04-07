4a4e11286666dda600e7eddbf5170a7579bc6275
David Kovari <dakovari@gmail.com>
2024-04-07 15:23:06 -0500

    remove testing env var from startup script

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-PEERS="192.168.100.11:64512,192.168.100.12:64512"
-
 bird-gen-config /etc/bird/bird.conf.tmpl /etc/bird/bird.conf
 
 #/usr/sbin/bird -d -u $BIRD_RUN_USER -g $BIRD_RUN_GROUP $BIRD_ARGS
